07317f9b9913dfdbc423b61c9a36376acdc02fe1
WarpedDemon <warped.demon.prince@hotmail.com>
2017-11-01 06:07:20 +0000

    Ready

--- PHPAPI.php
+++ PHPAPI.php
@@ -611,7 +611,7 @@ function AuthenticateUser($username, $password) {
 //Creates the database object used in many other api functions.
 function CreateConnectionObject() {
 	try {
-		$dsn = "mysql:host=106.68.129.105;dbname=appdatabase;charset=utf8;";
+		$dsn = "mysql:host=106.68.129.105:3306;dbname=appdatabase;charset=utf8;";
 		$db = new PDO($dsn, 'root', '');
 	}
 	catch(PDOException $e) {
